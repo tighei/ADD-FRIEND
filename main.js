79b5dc7c8569a116484d23358aab5f8711f745d8
tighei <123456789+odin@users.noreply.github.com>
2023-08-09 03:16:00 +0100

    make it render

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const lname = document.querySelector('#last-name');
 const OwnerLocation = document.querySelector('#place-owner');
 const SubmitBtn = document.querySelector('#submit');
 const myFriends = [];
+const cardContainer = document.querySelector('.card-container');
 
 addBtn.addEventListener('click', () => {
   form.style.display = 'flex';
@@ -20,15 +21,16 @@ class Friend {
 function render() {
   for (let i = 0; i < myFriends.length; i++) {
     const diva = document.createElement('div');
-    const image = document.createElement('img');
-    image.setAttribute('src', 'IMG_20221031_142636_805.jpg');
-    const fullName = document.createElement('h2');
-    fullName.innerHTML = `${Friend.n} `;
-
-    diva.append(image, fullName);
-
+    diva.innerHTML = `<img src="IMG_20221031_142636_805.jpg" alt="profile-pic" />
+    <h2>${myFriends[i].n} ${myFriends[i].l}</h2>
+    <div id="place-d">
+      <i class="fa-solid fa-location-dot"></i>
+      <p>${myFriends[i].lo}</p>
+    </div>
+    <input type="button" name="act-button" id="btn-act" value="Add Friend" />
+        `;
     diva.classList.add('card');
-    document.body.append(diva);
+    cardContainer.appendChild(diva);
     myFriends.splice(i, 1);
   }
 }
@@ -36,12 +38,14 @@ function render() {
 function addfriend() {
   const addfr = new Friend(fname.value, lname.value, OwnerLocation.value);
   myFriends.push(addfr);
-  console.log(myFriends);
-  console.log(lname.value);
   render();
 }
 
-SubmitBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+SubmitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
   addfriend();
+  form.style.display = 'none';
+  fname.value = '';
+  lname.value = '';
+  OwnerLocation.value = '';
 });
